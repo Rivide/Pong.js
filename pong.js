a8fc25f2f33422296c2b3e6eefcf1612e27c52f1
Rivide <rivide100@gmail.com>
2019-09-23 00:01:47 -0400

    Add ball bouncing and round end

--- pong.js
+++ pong.js
@@ -6,7 +6,7 @@ let height = 480;
 let paddleWidth = 10;
 let paddleHeight = 40;
 let paddleOffset = 60;
-let paddleSpeed = .2;
+let paddleSpeed = .25;
 
 let keys = {
     w: false,
@@ -35,34 +35,27 @@ class Player {
 let ball = {
     x: 0,
     y: 0,
-    speed: .2,
+    speed: .25,
     directionX: 0,
     directionY: 0,
     inPlay: false,
     width: 10,
     height: 10,
+    visible: true,
     setInPlay: function(x, y) {
-        this.x = x;
-        this.y = y;
-        let angle = Math.random() * 2 * Math.PI
-        this.directionX = Math.cos(angle);
-        this.directionY = Math.sin(angle);
+        this.visible = true;
+        this.x = width / 2 - this.width / 2;
+        this.y = Math.random() * (height - this.height);
+        let angle = Math.random() * 2 * Math.PI;
+        this.directionX = Math.sign(Math.random() - .5) * (Math.random() / 2 + .5);
+        this.directionY = Math.sign(Math.random() - .5) *
+        Math.sqrt(1 - Math.pow(this.directionX, 2));
         this.inPlay = true;
     },
     move: function(deltaTime) {
         this.x += this.directionX * this.speed * deltaTime;
         this.y += this.directionY * this.speed * deltaTime;
-        if (this.x < 0 || this.x + this.width > width || this.x < player1.x + player1.width &&
-            this.y + this.height > player1.y &&
-            this.y < player1.y + paddleHeight) {
-            this.directionX = -this.directionX;
-            if (this.x < 0) {
-                this.x = -this.x;
-            }
-            else {
-                this.x = width + width - this.x - this.width;
-            }
-        }
+
         if (this.y < 0 || this.y + this.height > height) {
             this.directionY = -this.directionY;
             if (this.y < 0) {
@@ -70,13 +63,33 @@ let ball = {
             }
             else {
                 this.y = height + height - this.y - this.height;
+                this.y -= 2 * (this.y + this.height - height);
+            }
+        }
+        if (this.x < player1.x + paddleWidth &&
+            this.x > player1.x &&
+            this.y + this.height > player1.y &&
+            this.y < player1.y + paddleHeight ||
+            this.x + this.width > player2.x && 
+            this.x + this.width < player2.x + paddleWidth &&
+            this.y + this.height > player2.y &&
+            this.y < player2.y + paddleHeight) {
+            this.directionX = -Math.sign(this.directionX) * (Math.random() / 2 + .5);
+            this.directionY = Math.sign(Math.random() - .5) *
+            Math.sqrt(1 - Math.pow(this.directionX, 2));
+            if (this.x < width / 2) {
+                this.x += 2 * (player1.x + paddleWidth - this.x);
+            }
+            else {
+                this.x -= 2 * (this.x + this.width - player2.x);
             }
         }
     },
     draw: function(ctx) {
-        console.log(this.x + " " + this.y + " " + this.width + " " + this.height);
-        ctx.fillStyle = "white";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        if (this.visible) {
+            ctx.fillStyle = "white";
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
 }
 
@@ -94,13 +107,19 @@ document.addEventListener("keyup", function(e) {
     }
 });
 let prevTime = 0;
+function draw() {
+
+}
 function update(timestamp) {
     let deltaTime = timestamp - prevTime;
     prevTime = timestamp;
 
     if (!ball.inPlay) {
-        ball.setInPlay(width / 2, Math.random() * height);
+        ball.setInPlay();
         console.log("inPlay");
+    }
+    if (ball.inPlay) {
+        ball.move(deltaTime);
     }
     if (keys.w) {
         player1.move(-1, deltaTime);
@@ -114,17 +133,23 @@ function update(timestamp) {
     if (keys.ArrowDown) {
         player2.move(1, deltaTime);
     }
+    if (ball.x < 0 || ball.x + ball.width > width) {
+        if (ball.x < 0) {
+            player2.score++;
+        }
+        else {
+            player1.score++;
+        }
+        console.log("Scores: " + player1.score + " - " + player2.score);
+        ball.visible = false;
+        ball.setInPlay();
+    }
 
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, width, height);
 
-    if (ball.inPlay) {
-        console.log("in Play for real");
-        ball.move(deltaTime);
-        ball.draw(ctx);
-    }
-
-    ctx.fillStyle = "white";
+    ball.draw(ctx);
+    
     player1.draw(ctx, paddleWidth, paddleHeight);
     player2.draw(ctx, paddleWidth, paddleHeight);
 
