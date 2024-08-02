506f4ff3342801839383e24e507995dae87dd369
Rivide <rivide100@gmail.com>
2024-08-02 19:01:14 -0400

    c

--- pong.js
+++ pong.js
@@ -25,10 +25,11 @@ class Player {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.direction = 0;
         this.score = 0;
     }
-    move(direction, deltaTime) {
-        this.y = Math.max(Math.min(this.y + paddleSpeed * direction *
+    update(deltaTime) {
+        this.y = Math.max(Math.min(this.y + paddleSpeed * this.direction *
             deltaTime, height - paddleHeight), 0);
     }
     draw(ctx, width, height) {
@@ -44,6 +45,7 @@ let ball = {
     inPlay: false,
     width: 10,
     height: 10,
+    lastHit: "none",
     setInPlay: function(x, y) {
         this.x = width / 2 - this.width / 2;
         this.y = Math.random() * (height - this.height);
@@ -55,6 +57,7 @@ let ball = {
     },
     setOutOfPlay() {
         this.inPlay = false;
+        this.lastHit = "none";
     },
     move: function(deltaTime) {
         this.x += this.directionX * this.speed * deltaTime;
@@ -66,28 +69,56 @@ let ball = {
                 this.y = -this.y;
             }
             else {
-                this.y = height + height - this.y - this.height;
-                this.y -= 2 * (this.y + this.height - height);
+                // this.y = height + height - this.y - this.height;
+                // this.y -= 2 * (this.y + this.height - height);
+                this.y = height - (this.y + this.height - height) - this.height;
             }
         }
-        if (this.x < player1.x + paddleWidth &&
+        // let ballVelocityX = this.speed * this.directionX;
+        // let ballVelocityYRelativeToLeftPaddle = this.speed * this.directionY - paddleSpeed;
+        if (
+            this.lastHit != "left" &&
+            this.x < player1.x + paddleWidth &&
             this.x > player1.x &&
             this.y + this.height > player1.y &&
-            this.y < player1.y + paddleHeight ||
+            this.y < player1.y + paddleHeight
+        ) {
+            this.directionX = Math.random() / 2 + .5;
+            this.directionY = Math.sign(Math.random() - .5) *
+            Math.sqrt(1 - Math.pow(this.directionX, 2));
+            this.lastHit = "left";
+        }
+        if (
+            this.lastHit != "right" &&
             this.x + this.width > player2.x && 
             this.x + this.width < player2.x + paddleWidth &&
             this.y + this.height > player2.y &&
-            this.y < player2.y + paddleHeight) {
-            this.directionX = -Math.sign(this.directionX) * (Math.random() / 2 + .5);
+            this.y < player2.y + paddleHeight
+        ) {
+            this.directionX = -(Math.random() / 2 + .5);
             this.directionY = Math.sign(Math.random() - .5) *
             Math.sqrt(1 - Math.pow(this.directionX, 2));
-            if (this.x < width / 2) {
-                this.x += 2 * (player1.x + paddleWidth - this.x);
-            }
-            else {
-                this.x -= 2 * (this.x + this.width - player2.x);
-            }
+            this.lastHit = "right";
         }
+        // if (
+        //     this.x < player1.x + paddleWidth &&
+        //     this.x > player1.x &&
+        //     this.y + this.height > player1.y &&
+        //     this.y < player1.y + paddleHeight ||
+        //     this.x + this.width > player2.x && 
+        //     this.x + this.width < player2.x + paddleWidth &&
+        //     this.y + this.height > player2.y &&
+        //     this.y < player2.y + paddleHeight) {
+        //     this.directionX = -Math.sign(this.directionX) * (Math.random() / 2 + .5);
+        //     this.directionY = Math.sign(Math.random() - .5) *
+        //     Math.sqrt(1 - Math.pow(this.directionX, 2));
+        //     if (this.x < width / 2) {
+        //         this.x += 2 * (player1.x + paddleWidth - this.x);
+        //     }
+        //     else {
+        //         this.x -= 2 * (this.x + this.width - player2.x);
+        //     }
+        // }
 
     },
     draw: function(ctx) {
@@ -110,22 +141,25 @@ document.addEventListener("keyup", function(e) {
         keys[e.key] = false;
     }
 });
-let prevTime = 0;
-function update(timestamp) {
-    let deltaTime = timestamp - prevTime;
-    prevTime = timestamp;
+function update(deltaTime) {
+    let player1Direction = 0;
     if (keys.w) {
-        player1.move(-1, deltaTime);
+        player1Direction -= 1;
     }
     if (keys.s) {
-        player1.move(1, deltaTime);
+        player1Direction += 1;
     }
+    let player2Direction = 0;
     if (keys.ArrowUp) {
-        player2.move(-1, deltaTime);
+        player2Direction -= 1;
     }
     if (keys.ArrowDown) {
-        player2.move(1, deltaTime);
+        player2Direction += 1;
     }
+    player1.direction = player1Direction;
+    player2.direction = player2Direction;
+    player1.update(deltaTime);
+    player2.update(deltaTime);
     if (!ball.inPlay) {
         timeToRound -= deltaTime;
         if (timeToRound <= 0) {
@@ -162,7 +196,14 @@ function update(timestamp) {
     
     player1.draw(ctx, paddleWidth, paddleHeight);
     player2.draw(ctx, paddleWidth, paddleHeight);
-
-    requestAnimationFrame(update);
 }
-requestAnimationFrame(update);
+
+let prevTime = 0;
+function loop(timestamp) {
+    let deltaTime = timestamp - prevTime;
+    // let deltaTime = 1;
+    prevTime = timestamp;
+    update(deltaTime)
+    requestAnimationFrame(loop);
+}
+requestAnimationFrame(loop);
