4741cd4f9d1073ffb73a6ed67fa4b7b9c9985339
Rivide <rivide100@gmail.com>
2024-08-06 23:38:44 -0400

    c

--- client.js
+++ client.js
@@ -12,6 +12,11 @@ let paddleOffset = 60;
 let scoreOffsetX = 80;
 let scoreOffsetY = 30;
 
+const playButtonX = width / 2;
+const playButtonY = 220;
+const multiplayerButtonX = width / 2;
+const multiplayerButtonY = 260;
+
 let keys = {
     w: false,
     s: false,
@@ -55,6 +60,21 @@ document.addEventListener("keyup", function(e) {
     if (keys.hasOwnProperty(e.key)) {
         keys[e.key] = false;
     }
+});
+
+let mouseX = -1;
+let mouseY = -1;
+canvas.addEventListener("mousemove", function(e) {
+  mouseX = e.offsetX;
+  mouseY = e.offsetY;
+});
+
+let clickEvents = [];
+canvas.addEventListener("click", function(e) {
+  clickEvents.push({
+    x: e.offsetX,
+    y: e.offsetY
+  });
 });
 
 function updatePlayer(clientPlayer, serverPlayer) {
@@ -116,9 +136,40 @@ function loop(timestamp) {
   ctx.font = "40px Arial";
   fillTextCenteredHorizontally(ctx, "Pong", width / 2, 140);
   ctx.font = "30px Arial";
-  fillTextCenteredHorizontally(ctx, "Play", width / 2, 220);
-  fillTextCenteredHorizontally(ctx, "Multiplayer", width / 2, 260);
 
+  const playButtonCoords = textCoords(ctx, "Play", playButtonX, playButtonY);
+  const multiplayerButtonCoords = textCoords(ctx, "Multiplayer", multiplayerButtonX, multiplayerButtonY);
+  for (const clickEvent of clickEvents) {
+    if (checkPointInside(
+      clickEvent.x,
+      clickEvent.y,
+      playButtonCoords.x0,
+      playButtonCoords.y0,
+      playButtonCoords.x1,
+      playButtonCoords.y1
+    )) {
+      console.log("PLAY");
+      break;
+    }
+    if (checkPointInside(
+      clickEvent.x,
+      clickEvent.y,
+      multiplayerButtonCoords.x0,
+      multiplayerButtonCoords.y0,
+      multiplayerButtonCoords.x1,
+      multiplayerButtonCoords.y1
+    )) {
+      console.log("MULTIPLAYER");
+      break;
+    }
+  }
+
+  drawButton(ctx, "Play", playButtonX, playButtonY);
+  drawButton(ctx, "Multiplayer", multiplayerButtonX, multiplayerButtonY);
+  
+  // fillTextCenteredHorizontally(ctx, "Play", width / 2, 220);
+  // fillTextCenteredHorizontally(ctx, "Multiplayer", width / 2, 260);
+  clickEvents = [];
 //   update(deltaTime)
   requestAnimationFrame(loop);
 }
@@ -127,4 +178,43 @@ requestAnimationFrame(loop);
 function fillTextCenteredHorizontally(ctx, text, x, y) {
     const width = ctx.measureText(text).width;
     ctx.fillText(text, x - width / 2, y);
+}
+
+function textCoords(ctx, text, x, y) {
+  const metrics = ctx.measureText(text);
+  return {
+    x0: x - metrics.width / 2,
+    x1: x + metrics.width / 2,
+    y0: y - metrics.actualBoundingBoxAscent,
+    y1: y + metrics.actualBoundingBoxDescent
+  };
+}
+
+function checkPointInside(x, y, x0, y0, x1, y1) {
+  return x > x0 && x < x1 && y > y0 && y < y1;
+}
+
+function drawButton(ctx, text, x, y) {
+  const metrics = ctx.measureText(text);
+  const width = metrics.width;
+  // const height = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+  const x0 = x - width / 2;
+  const x1 = x + width / 2;
+  const y0 = y - metrics.actualBoundingBoxAscent;
+  const y1 = y + metrics.actualBoundingBoxDescent;
+
+  if (
+    mouseX > x0 && mouseX < x1 &&
+    mouseY > y0 && mouseY < y1
+  ) {
+    ctx.fillStyle = "lightgray";
+  } else {
+    ctx.fillStyle = "white";
+  }
+  ctx.fillText(text, x - width / 2, y);
+  // ctx.strokeStyle = "red";
+  // ctx.beginPath();
+  // ctx.moveTo(x0, y1);
+  // ctx.lineTo(x1, y1);
+  // ctx.stroke();
 }
--- notes.md
+++ notes.md
@@ -1,1 +1,9 @@
 https://socket.io/docs/v4/tutorial/step-1
+## Events
+``` js
+canvas.addEventListener("click", function(e) {
+  // Mouse position relative to the element.
+  e.offsetX;
+  e.offsetY;
+});
+```
